4c35d2682125f9bc198987ee7d8dd0053bab8849
Fouink <yyss9103@gmail.com>
2022-11-30 15:47:28 +0900

    start loadBalncing

---
+++ module_userInfo/src/main/java/com/fouink/userinfo/UserInfoApplication.java
@@ -0,0 +1,13 @@
+package com.fouink.userinfo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+@SpringBootApplication
+@EnableDiscoveryClient
+public class UserInfoApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(UserInfoApplication.class, args);
+    }
+}
