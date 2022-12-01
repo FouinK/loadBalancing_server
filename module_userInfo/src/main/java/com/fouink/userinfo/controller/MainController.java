86997c65a719bb2d6413e949426327748657187d
Fouink <yyss9103@gmail.com>
2022-12-01 10:19:19 +0900

    #1 회원가입 api 구현

--- module_userInfo/src/main/java/com/fouink/userinfo/controller/MainController.java
+++ module_userInfo/src/main/java/com/fouink/userinfo/controller/MainController.java
@@ -1,14 +1,26 @@
 package com.fouink.userinfo.controller;
 
+import com.fouink.userinfo.dto.UserJoinRequestApi;
+import com.fouink.userinfo.entity.UserInfo;
+import com.fouink.userinfo.service.UserService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequiredArgsConstructor
 public class MainController {
 
-    @GetMapping("/")
-    public ResponseEntity<?> userInfo() {
-        return ResponseEntity.ok("hi");
+    private final UserService userService;
+
+    @PostMapping("/join")
+    public ResponseEntity<?> userJoin(@RequestBody UserJoinRequestApi requestApi) {
+
+        UserInfo joinUserInfo = userService.join(requestApi);
+
+        return ResponseEntity.ok(joinUserInfo);
     }
 }
--- module_userInfo/src/main/java/com/fouink/userinfo/entity/BaseTimeEntity.java
+++ module_userInfo/src/main/java/com/fouink/userinfo/entity/BaseTimeEntity.java
@@ -1,6 +1,7 @@
 package com.fouink.userinfo.entity;
 
 import lombok.Getter;
+import lombok.Setter;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
@@ -12,6 +13,7 @@ import java.time.LocalDateTime;
 @Getter
 @MappedSuperclass//jpa가 클래스를 상속할 경우 필드들도 칼럼으로 인식
 @EntityListeners(AuditingEntityListener.class)//클래스에 Auditing 기능(jpa에서 시간테이블 자동으로 넣음) 포함
+@Setter
 public class BaseTimeEntity {
     @CreatedDate//엔티티저장될 때 자동으로 시간도 저장
     private LocalDateTime createdDate;
